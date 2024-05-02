55dd36cf8e4ad63bcfbd11bdceefe967da6b4f98
adhikarisaroj795 <58041950+adhikarisaroj795@users.noreply.github.com>
2024-05-02 23:05:05 +0545

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 //handling cors policy
 const corsOptions = {
-  origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
+  origin: ["https://react-portfolio-navy-sigma.vercel.app/"],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
